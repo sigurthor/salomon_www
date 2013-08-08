89bc2ad4bbb792f3cdd0ce3c23db73073340edfd
Sigurthor <sigurthor.einar@gmail.com>
2013-08-08 17:11:53 +0000

    clean project with foundation

---
+++ app/controllers/team_controller.rb
@@ -0,0 +1,7 @@
+class TeamController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalomonWww::Application.config.secret_token = 'edd542810349cdd5d1c7533098fb37b4a1325784eb91373401c55ad4aee714d2b5259350d7106ae81ed54b660d1728f8d2cdc365ecaf0b834226739cb0814de7'
