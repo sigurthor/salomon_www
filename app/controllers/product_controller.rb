03de84e09b9ae0c7319f9a3d3b6a933e095373d7
Sigurthor <sigurthor.einar@gmail.com>
2013-08-14 01:41:39 +0000

    Product category list from DB

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,9 @@
 class ProductController < BaseController
   def index
-    @products = A2::Product.all
+
+    category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
+    @products = category.products.includes(product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
+
   end
 
   def show
