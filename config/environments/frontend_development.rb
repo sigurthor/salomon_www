2d72b128ee93f558725d5f8c6b8119b9cd9091b8
Sigurthor <sigurthor.einar@gmail.com>
2013-08-16 15:16:46 +0000

    Product index query fix

--- config/environments/frontend_development.rb
+++ config/environments/frontend_development.rb
@@ -4,5 +4,6 @@ SalomonWww::Application.configure do
   #config.identity_cache_store = :mem_cache_store, Memcached::Rails.new(:servers => ["127.0.0.1"])
 
   #Expands the lines which load the assets
+  config.active_record.auto_explain_threshold_in_seconds = nil
   config.assets.debug = false
 end
