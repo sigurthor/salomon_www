31c2f01e96e3c48bb1e67ad4194ac9b500a65037
Sigurthor <sigurthor.einar@gmail.com>
2013-08-16 21:20:11 +0000

    Cache to product index

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,8 +1,10 @@
 class ProductController < BaseController
+
+
   def index
-    category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
-    @feature_types = A2::ProductFeatureType.includes(:product_features).where(:feature_type_code => category.filters.split(','))
-    @products = category.products.includes(product_variants: [:product_image],product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
+    @category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
+    @feature_types = A2::ProductFeatureType.includes(:product_features).where(:feature_type_code => @category.filters.split(','))
+    @products = @category.products.includes(product_variants: [:product_image],product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
 
   end
 
