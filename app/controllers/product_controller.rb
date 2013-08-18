f5d2585a54e3e36e53340534aec928f23a54ecd6
Sigurthor <sigurthor.einar@gmail.com>
2013-08-18 21:43:16 +0000

    Product category include fix

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -4,7 +4,7 @@ class ProductController < BaseController
   def index
     @category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
     @feature_types = A2::ProductFeatureType.includes(:product_features).where(:id => @category.filters.split(',')) unless @category.filters.blank?
-    @products = @category.products.includes([:product_image],product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
+    @products = @category.products.includes({product_color_variants: :product_images},{product_features: :product_feature_type},{product_variants: :product_image})   #todo use scope
 
   end
 
