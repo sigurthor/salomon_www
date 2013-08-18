1b63edf202cfa16272de052ff5868516624910e0
Sigurthor <sigurthor.einar@gmail.com>
2013-08-18 21:19:36 +0000

    Team on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @vimeo = A2::VimeoFeed.first
+
+    team_categorys = A2::TeamCategory.find_by_slug('salomon').descendants
+    @team_categories_info =  team_categorys.map {|c| {name: c.name,count: c.team_members.count}}
+    @pro_team_members = team_categorys.map {|c| c.team_members.limit(7) if c.slug == 'pro'}.first
+
   end
 
   def contact
