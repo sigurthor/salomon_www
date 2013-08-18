24fda6bfbd1f24f97d3661d91ab47f6521eacf66
Sigurthor <sigurthor.einar@gmail.com>
2013-08-18 20:07:59 +0000

    Product query fix

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -4,11 +4,11 @@ class ProductController < BaseController
   def index
     @category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
     @feature_types = A2::ProductFeatureType.includes(:product_features).where(:id => @category.filters.split(',')) unless @category.filters.blank?
-    @products = @category.products.includes(product_variants: [:product_image],product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
+    @products = @category.products.includes([:product_image],product_features: [:product_feature_type],product_color_variants: [:product_images])   #todo use scope
 
   end
 
   def show
-    @product = A2::Product.find_by_slug(params[:product])
+    @product = A2::Product.includes({product_color_variants: :product_images},{product_features: :product_feature_type},{product_variants: :product_image},{product_variants: {tech_specifications: :tech_definition}}).where(:slug => params[:product]).limit(1).first
   end
 end
