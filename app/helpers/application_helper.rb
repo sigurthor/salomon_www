9271abf0f3c54ff744f4528d6d75aba6e3147280
Sigurthor <sigurthor.einar@gmail.com>
2013-08-20 21:34:06 +0000

    Snowboard  image crop fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def set_text_length_class(filter_name, length)
     {:class => (filter_name.length > length ? 'long' : '')}
   end
+
+  def prepend_snowboard_crop(image_url)
+     image_url.gsub!('upload/','upload/c_crop,g_north_west,w_954,h_5000/').html_safe
+  end
 end
