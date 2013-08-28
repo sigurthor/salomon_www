722c8a00768cb7d3dd1ccc58239ad2c5bea67b90
Sigurthor <sigurthor.einar@gmail.com>
2013-08-28 13:58:34 +0000

    countries menu

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -2,4 +2,8 @@ module LayoutHelper
   def gear_categories
     @menu_items ||= A2::ProductCategory.where(:visible => true).order(:position)
   end
+
+  def regions
+    @regions ||= A2::Region.all
+  end
 end
