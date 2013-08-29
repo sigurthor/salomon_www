3f6476da921068d62ba9ade2226882819a368885
Ólafur Örn Nielsen <olafur@form5.is>
2013-08-29 19:52:57 +0000

    Adding Mustache templates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ SalomonWww::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( custom.modernizr.js html5shiv.js form5image.js )
+  config.assets.precompile += %w( custom.modernizr.js html5shiv.js form5image.js mustache.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
