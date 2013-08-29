a58aea1b3ab0df7cab3eb9a3bf56f2bf3d996c05
Sigurthor <sigurthor.einar@gmail.com>
2013-08-29 19:50:13 +0000

    query change

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -5,7 +5,7 @@ class ProductController < BaseController
     @category = A2::ProductCategory.by_slug(params[:category]).first  #todo cache
     page @category.slug
     @feature_types = A2::ProductFeatureType.includes(:product_features).where(:id => @category.filters.split(',')) unless @category.filters.blank?
-    @products = @category.products.includes({product_color_variants: :product_images},{product_features: :product_feature_type},{product_variants: :product_image})   #todo use scope
+    @products = @category.products.includes(:categories, {product_color_variants: :product_images},{product_features: :product_feature_type},{product_variants: :product_image})   #todo use scope
 
   end
 
--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -15,9 +15,8 @@ class TeamController < BaseController
 
   def show
     page 'team-member'
-    @team_categories = A2::TeamCategory.find_by_slug('salomon').descendants
+    @team_categories = A2::TeamCategory.find_by_slug('salomon').descendants  unless params[:ajax]
     @team_member = A2::TeamMember.find_by_slug(params[:team_member])
-    @total_riders = @team_member.category.team_members.count
     @profile_video = @team_member.video
 
     if params[:ajax]
