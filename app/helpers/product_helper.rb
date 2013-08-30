f0de13609043f6a8a3da250c8a1b6ebec754aceb
Sigurthor <sigurthor.einar@gmail.com>
2013-08-30 14:02:47 +0000

    product cache A2::UPDATE

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,14 +1,14 @@
 module ProductHelper
 
   def get_size_variants
-    unless @product.product_variants.count == 1
+    unless @product.product_variants.length == 1
       render :partial => 'product_size_thumbs', :locals =>
       { :variants => @product.product_variants }
     end
   end
 
   def get_color_variants
-    unless @product.product_color_variants.count == 1
+    unless @product.product_color_variants.length == 1
       render :partial => 'product_color_thumbs',
              :locals => { :variants => @product.product_color_variants }
     end
