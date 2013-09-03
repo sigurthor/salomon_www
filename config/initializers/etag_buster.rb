81caa3c2cc0c3642f29826bdaca952283e201c25
Sigurthor <sigurthor.einar@gmail.com>
2013-09-03 10:12:07 +0000

    etax app_name fix

--- config/initializers/etag_buster.rb
+++ config/initializers/etag_buster.rb
@@ -2,6 +2,6 @@ require 'heroku-api'
 
 if Rails.env.production?
   heroku = Heroku::API.new(:api_key => Rails.application.config.heroku_api)
-  release_version = heroku.get_releases('myappname').body.last
+  release_version = heroku.get_releases(ENV['APP_NAME']).body.last
   ENV["ETAG_VERSION_ID"] = release_version["name"]
 end
