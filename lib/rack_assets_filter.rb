cb760b56a2be1fe295cc634ed28c9ba1cc7b445a
sigurthor <sigurthor.einar@gmail.com>
2013-09-11 17:27:31 +0000

    remove outputs from rack filter

--- lib/rack_assets_filter.rb
+++ lib/rack_assets_filter.rb
@@ -5,12 +5,8 @@ class RackAssetsFilter
 
   def call(env)
     status, headers, body = @app.call(env)
-    puts env
-    puts env['PATH_INFO']
     if env['REQUEST_PATH'].starts_with?("/assets/webfonts")
-      puts "add origin"
       headers['Access-Control-Allow-Origin'] = '*'
-      # ...
     end
     [status, headers, body]
   end
