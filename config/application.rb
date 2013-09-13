4dad0b13fb23fb3fb318c13c20b7f8a48ca63c0d
sigurthor <sigurthor.einar@gmail.com>
2013-09-13 13:47:48 +0000

    version bump

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,6 @@ module SalomonWww
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.6'
+    config.assets.version = '1.7'
   end
 end
