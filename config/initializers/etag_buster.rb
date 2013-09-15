a4f4086604aac46031ec0e0131cb207020d1b6ec
sigurthor <sigurthor.einar@gmail.com>
2013-09-15 08:34:17 +0000

    heroku fix

--- config/initializers/etag_buster.rb
+++ config/initializers/etag_buster.rb
@@ -1,7 +1,7 @@
 require 'heroku-api'
 
 if Rails.env.production? || Rails.env.staging?
-  heroku = Heroku::API.new(:api_key => Rails.application.config.heroku_api)
-  release_version = heroku.get_releases(ENV['APP_NAME']).body.last
-  ENV["ETAG_VERSION_ID"] = release_version["name"]
+  #heroku = Heroku::API.new(:api_key => Rails.application.config.heroku_api)
+  #release_version = heroku.get_releases(ENV['APP_NAME']).body.last
+  ENV["ETAG_VERSION_ID"] = '23' #release_version["name"]
 end
