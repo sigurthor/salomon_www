e36ef7cf0dcd88261f35de3f9cba6a74ba0dbde5
sigurthor <sigurthor.einar@gmail.com>
2013-10-23 11:13:23 +0000

    asset compress fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ SalomonWww::Application.configure do
   config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
